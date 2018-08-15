c127761dbd295c5455d0d455ab723c93dc51c366
hyjr <shenhui@huiepay.com>
2018-08-15 16:54:53 +0800

    20180815
    第二次

---
+++ api/config/main.php
@@ -0,0 +1,64 @@
+<?php
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
+);
+
+return [
+    'id' => 'app-api',
+    'timeZone' => 'Asia/Shanghai',
+    'basePath' => dirname(__DIR__),
+    'controllerNamespace' => 'api\controllers',
+    'bootstrap' => ['log'],
+    'modules' => [],
+    'components' => [
+        'authManager' => [
+            'class' => 'app\rbac\DbManager',
+        ],
+       /* 'i18n' => [
+            'translations' => [
+                'app' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@common/messages'
+                ],
+            ],
+        ],*/
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => false,
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'formatter' => [
+            'class' => 'app\utils\Formatter',
+            'datetimeFormat' => 'yyyy-MM-dd HH:mm:ss',
+            'defaultTimeZone' => 'Asia/Shanghai',
+        ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+            ],
+        ],
+
+        'request' => [
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+                'text/json' => 'yii\web\JsonParser',
+            ],
+        ],
+    ],
+    'params' => $params,
+];
